0f03405e02a2e8de0394592fb50d88df2b99c7ae
Aamer <114329665+Aamer304@users.noreply.github.com>
2022-11-05 22:40:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let CountEl=document.getElementById("count-el")
+let count=0
+function  increment()
+{
+    count=count+1
+    CountEl.innerText=count
+}
+let car=document.getElementById("count-el")
+function resett(){
+    count=0
+    CountEl.innerText=count
+}
